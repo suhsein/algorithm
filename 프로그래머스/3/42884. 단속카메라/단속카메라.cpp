3c0967004fdaef8a2bef84c2126b22a17b9109e6
suhsein <76998096+suhsein@users.noreply.github.com>
2024-03-22 00:22:54 +0900

    Update 단속카메라.cpp

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -6,19 +6,15 @@
 using namespace std;
 
 bool cmp(vector<int> &a, vector<int> & b){
-    if(a[1] == b[1]) return a[0] < b[0];
-    return a[1] < b[1];
+    if(a[1] == b[1]) return a[0] < b[0]; // 진출 같으면 진입 빠른 순
+    return a[1] < b[1]; // 진출 빠른 순
 }
 int solution(vector<vector<int>> routes) {
     sort(routes.begin(), routes.end(), cmp);
     int answer = 0;
 
     for(int i = 0, j = i + 1; i < routes.size(); i = j, j = i+1){
-        int l = routes[i][1];
-        while(j < routes.size() && l >= routes[j][0]) {
-            j++;
-            if(j < routes.size()) l = min(l, routes[j][1]);
-        } 
+        while(j < routes.size() && routes[i][1]; >= routes[j][0]) j++;
         answer++;
     }
     return answer;
