2c9a2ba800f9bc8dc377b4db7fba1f681ddc06b4
suhsein <76998096+suhsein@users.noreply.github.com>
2024-03-25 21:25:36 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(auto &c: myString)
+        if(c <= 'Z') c += ('a' - 'A');   
+    return myString;
+}
