595a50f20921d2847e1f5b58d38ab635a46995a2
suhsein <76998096+suhsein@users.noreply.github.com>
2024-03-25 20:49:10 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    answer = my_string.substr(0, s);
+    answer += overwrite_string;
+    answer += my_string.substr(s+overwrite_string.size());
+    return answer;
+}
