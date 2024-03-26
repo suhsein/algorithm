b41323bfc99ce879efd0a79f052c590b73906713
suhsein <76998096+suhsein@users.noreply.github.com>
2024-03-26 22:08:28 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
@@ -1,25 +1,10 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int arr[3];
+int a, b, c;
 int main(void) {
 	ios::sync_with_stdio(0), cin.tie(0);
-	while (cin >> arr[0]) {
-		cin >> arr[1] >> arr[2];
-		int cnt = 0;
-		while (1) {
-			sort(arr, arr + 3);
-			int gap1 = arr[1] - arr[0], gap2 = arr[2] - arr[1];
-			if (gap1 >= gap2) {
-				arr[2] = arr[1] - 1;
-				if (arr[2] == arr[0]) break;
-			}
-			else {
-				arr[0] = arr[2] - 1;
-				if (arr[0] == arr[1]) break;
-			}
-			cnt++;
-		}
-		cout << cnt << "\n";
+	while (cin >> a >> b >> c) {
+		if (c - b > b - a) cout << c - b - 1 << "\n";
+		else cout << b - a - 1 << "\n";
 	}
 }
