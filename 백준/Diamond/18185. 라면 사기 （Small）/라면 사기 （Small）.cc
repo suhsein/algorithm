8d708355670d6b69eb62bb10ba0a24d423f1cf25
suhsein <76998096+suhsein@users.noreply.github.com>
2024-03-26 20:03:08 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#define ll long long
+using namespace std;
+int N, b, c;
+ll ans, cur, B, C1, C2;
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0);
+	cin >> N;
+
+	// B 현재 공장에서 b 금액으로 사야만하는 라면 갯수
+	// C1 다음 공장에서 c 금액으로 살 수 있는 라면 갯수
+	// C2 다다음 공장에서 c 금액으로 살 수 있는 라면 갯수
+    b = 3, c = 2;
+	for (int i = 0; i < N; i++) {
+		cin >> cur;
+		B = cur - min(C1, cur); // C로 바꾸고 남은 나머지
+		ans += (B * b) + (min(C1, cur) * c); 
+		C1 = min(C2, cur) + B; // 순서 + 1 -> 다다음 공장이 다음 공장이 되야하므로
+		C2 = B;
+	}
+	cout << ans;
+}
