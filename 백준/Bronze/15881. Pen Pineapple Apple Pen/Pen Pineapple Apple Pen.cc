f66596c6d9fca895ffef18d7c68d365ba9c6e3e3
suhsein <76998096+suhsein@users.noreply.github.com>
2024-03-27 18:59:01 +0900

    [Bronze I] Title: Pen Pineapple Apple Pen, Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/15881. Pen Pineapple Apple Pen/Pen Pineapple Apple Pen.cc
+++ 백준/Bronze/15881. Pen Pineapple Apple Pen/Pen Pineapple Apple Pen.cc
@@ -6,14 +6,13 @@ string ppap, PPAP= "PAp";
 int main(void) {
 	ios::sync_with_stdio(0), cin.tie(0);
 	cin >> n >> ppap;
-	int sz = ppap.size();
-	for (int l = 0, r = 1, cnt = 0; l < sz - 3; l++, r = l + 1, cnt = 0) {
+	for (int l = 0, r = 1, cnt = 0; l < n - 3; l++, r = l + 1, cnt = 0) {
 		if (ppap[l] != 'p') continue;
-		while (l < sz - 3) {
+		while (l < n - 3) {
 			idx = 0;
-			while (r < sz && ppap[r] == PPAP[idx]) { r++; idx++; }
+			while (r < n && ppap[r] == PPAP[idx]) { r++; idx++; }
 			if (idx == 3) cnt++;
-			while (r < sz - 3 && ppap[r] != 'p') r++;
+			while (r < n - 3 && ppap[r] != 'p') r++;
 			l = r; r = l + 1;
 		}
 		if (cnt > ans) ans = cnt;
