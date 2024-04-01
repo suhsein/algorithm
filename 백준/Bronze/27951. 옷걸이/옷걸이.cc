4dd4d0410a43c3bde5ce04aed86dbd7bda74ec8d
suhsein <76998096+suhsein@users.noreply.github.com>
2024-04-01 20:33:06 +0900

    [Bronze I] Title: 옷걸이, Time: 68 ms, Memory: 8936 KB, Score: 23 point -BaekjoonHub

---
+++ 백준/Bronze/27951. 옷걸이/옷걸이.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#define MX 1000005
+using namespace std;
+int N, U, D;
+string ans;
+int arr[MX];
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> arr[i];
+	cin >> U >> D;
+
+	for (int i = 0; i < N; i++) {
+		if (arr[i] == 1) {
+			if (U) {
+				U--;
+				ans += "U";
+			}
+		}
+		else if (arr[i] == 2) {
+			if (D) {
+				D--;
+				ans += "D";
+			}
+		}
+		else if (arr[i] == 3) {
+			if (U) {
+				U--;
+				ans += "U";
+			} else if (D) {
+				D--;
+				ans += "D";
+			}
+		}
+	}
+
+	if (U || D) cout << "NO";
+	else cout << "YES\n" << ans;
+	
+}
