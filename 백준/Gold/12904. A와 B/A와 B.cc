abafddc0a56fd080d00cd85b1e6f2981e1c68cf9
suhsein <76998096+suhsein@users.noreply.github.com>
2024-04-04 21:39:19 +0900

    [Gold V] Title: A와 B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.cc
+++ 백준/Gold/12904. A와 B/A와 B.cc
@@ -1,40 +1,22 @@
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 #include <string>
+#include <algorithm>
 using namespace std;
 string s, t;
-bool flag;
-int ss, sa, sb, ta, tb;
-unordered_map<string, bool> m;
-
-void solve(string str, int cura, int curb) {
-	if (flag || (m.find(str) != m.end())) return;
-	if (str == s) {
-		flag = 1;
-		return;
-	}
-	m[str] = 1;
-	if (cura > sa && str[str.size() - 1] == 'A') 
-		solve(str.substr(0, str.size() - 1), cura - 1, curb);
-	if (curb > sb && str[str.size() - 1] == 'B') {
-		string tmp = str.substr(0, str.size() - 1);
-		reverse(tmp.begin(), tmp.end());
-		solve(tmp, cura, curb - 1);
-	}
-}
 int main(void) {
 	ios::sync_with_stdio(0), cin.tie(0);
 	cin >> s >> t;
-	ss = s.size();
-	for (auto x : s) {
-		if (x == 'A') sa++;
-		else if (x == 'B') sb++;
+	while (1) {
+		if (t.back() == 'A') {
+			t.pop_back();
+		}
+		else if (t.back() == 'B') {
+			t.pop_back();
+			reverse(t.begin(), t.end());
+		}
+		if (t.size() == s.size()) {
+			cout << (t == s ? 1 : 0);
+			return 0;
+		}
 	}
-	for (auto x : t) {
-		if (x == 'A') ta++;
-		else if (x == 'B') tb++;
-	}
-	solve(t, ta, tb);
-	cout << flag;
 }
