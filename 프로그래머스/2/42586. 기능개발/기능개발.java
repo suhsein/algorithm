127ab83770ebec717970c6f2179c1cf9afa21476
suhsein <mini9075@naver.com>
2025-02-26 15:58:04 +0900

    [level 2] Title: 기능개발, Time: 2.91 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,30 +1,37 @@
 import java.util.*;
 
 class Solution {
+    int n;
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList<>();
-        int n = progresses.length;
+        n = progresses.length;
        
-        for(int i = 0; i < n; i++){
+        int[] days = getDays(progresses, speeds);
+        
+        int cur = 0, nxt = cur + 1;
+        
+        while(nxt < n){
+          if(days[cur] >= days[nxt]) {
+              nxt++;
+          } else {
+              answer.add(nxt - cur);
+              cur = nxt;
+              nxt = cur + 1;
+          }
+        }
+        answer.add(nxt - cur);
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+    
+    public int[] getDays(int[] progresses, int[] speeds){
+        int[] days = new int[n];
+         for(int i = 0; i < n; i++){
             int r = 100 - progresses[i];
             int t = r / speeds[i];
             if(r % speeds[i] != 0) t++;
-            
-            int tmp = i + 1;
-        
-            for(int j = i + 1; j < n; j++){
-                int nr = 100 - progresses[j];
-                int nt = nr / speeds[j];
-                if(nr % speeds[j] != 0) nt++;
-                if (t < nt) break;
-                tmp++;
-            }
-        
-            answer.add(tmp - i);
-            i = tmp - 1;
-        }
-        
-        
-        return answer.stream().mapToInt(i->i).toArray();
+            days[i] = t;
+         }
+        return days;
     }
 }
