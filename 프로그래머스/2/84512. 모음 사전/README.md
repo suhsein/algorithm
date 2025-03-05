988ebbfdc557b0351bb7b20d49e65a7e0fd0c632
suhsein <mini9075@naver.com>
2025-03-05 11:24:15 +0900

    [level 2] Title: 모음 사전, Time: 0.05 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,6 +1,6 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=cpp) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 11:23:46
+2025년 03월 05일 11:24:12
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,27 +1,15 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
+
 using namespace std;
 
-string arr[6]= {"A", "E", "I", "O", "U"};
-string w;
-int cnt = 0, ans = 0;
-
-void dfs(string s){
-    cnt++;
-    if(ans != 0) return;
-    if(s == w) {
-        ans = cnt;
-        return;
-    }
-    if(s.size() == 5) return;
-
-    for(int i = 0; i < 5; i++) dfs(s + arr[i]);
-}
-
 int solution(string word) {
-    w = word;
-    dfs("");
-    return ans - 1;
+    string s = "AEIOU";
+    int arr[] = { 781, 156, 31, 6, 1 }, answer = 0;
+    
+    for(int i = 0; i < word.size(); i++)
+        answer += s.find(word[i]) * arr[i] + 1; 
+    
+    return answer;
 }
