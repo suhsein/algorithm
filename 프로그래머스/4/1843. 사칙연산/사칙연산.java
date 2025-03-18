37f50b5f341bcde23097505f53290530095522fd
suhsein <mini9075@naver.com>
2025-03-18 13:34:50 +0900

    [level 4] Title: 사칙연산, Time: 34.88 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -3,26 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(String arr[]) {
         int n = arr.length / 2 + 1;
-        int[][] mn = new int[n][n];
-        int[][] mx = new int[n][n];
         
-        for(int i = 0; i < n; i++) {
-            mn[i][i] = Integer.parseInt(arr[i * 2]);
-            mx[i][i] = Integer.parseInt(arr[i * 2]);
-        }
+        int[][] mx = new int[n][n];
+        int[][] mn = new int[n][n];
+        
+        for(int i = 0; i <  n; i++)
+            mx[i][i] = mn[i][i] = Integer.parseInt(arr[i * 2]);            
         
         for(int len = 1; len < n; len++){
-            for(int i = 0; i < n - len; i++) {
+            for(int i = 0; i < n - len; i++){
                 int j = i + len;
                 mx[i][j] = Integer.MIN_VALUE;
                 mn[i][j] = Integer.MAX_VALUE;
                 
-                for(int k = i; k < j; k++) {
+                for(int k = i; k < j; k++){
                     String op = arr[k * 2 + 1];
-                   
                     int mxLeft = mx[i][k];
-                    int mnLeft = mn[i][k];
                     int mxRight = mx[k + 1][j];
+                    int mnLeft = mn[i][k];
                     int mnRight = mn[k + 1][j];
                     
                     if(op.equals("-")) {
@@ -30,8 +28,9 @@ class Solution {
                         mn[i][j] = Math.min(mn[i][j], mnLeft - mxRight);
                     } else {
                         mx[i][j] = Math.max(mx[i][j], mxLeft + mxRight);
-                        mn[i][j] = Math.min(mn[i][j], mnLeft + mnRight);
-                    } 
+                        mn[i][j] = Math.min(mn[i][j], mnLeft + mnRight);    
+                    }
+                    
                 }
             }
         }
