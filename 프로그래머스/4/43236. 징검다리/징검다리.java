dac607e35fa97f55df5959723ffbb921a4da5e5b
suhsein <mini9075@naver.com>
2025-03-20 12:19:13 +0900

    [level 4] Title: 징검다리, Time: 45.48 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -6,22 +6,27 @@ class Solution {
         
         Arrays.sort(rocks);
        
-        int lo = 0, hi = distance + 1, m = 0, cnt = 0, prev = 0;
+        int lo = 0, hi = distance + 1, m = 0;
       
         while(lo + 1 < hi) {
             m = (lo + hi) / 2;
-            cnt = 0; prev = 0;
             
-            for(int r : rocks) {
-                if(r - prev < m) cnt++;
-                else prev = r;
-            }
-            
-            if(distance - prev < m) cnt++;
-            if(cnt > n) hi = m;
+            if(check(m, n, distance, rocks)) hi = m;
             else lo = m;
         }
         
         return lo;
+    }
+    
+    public boolean check(int m, int n, int distance, int[] rocks) {
+        int cnt = 0, prev = 0;
+        
+        for(int r : rocks) {
+            if(r - prev < m) cnt++;
+            else prev = r;
+            if(cnt > n) return true;
+        }
+        if(distance - prev < m) cnt++;
+        return cnt > n;
     }
 }
