cfb0e0b871e7b9ed5a62c92a199adb8a6b259f76
suhsein <mini9075@naver.com>
2025-03-25 20:24:49 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 124 ms, Memory: 15840 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final int DWARFS = 9;
+    static boolean[] visit = new boolean[DWARFS];
+    static int[] arr;
+    static BufferedWriter bw;
+    static boolean flag = false; 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  
+        arr = new int[DWARFS];
+        for(int i = 0; i < DWARFS; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        
+        dfs(0, 0);
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    public static void dfs(int tot, int cnt) throws IOException {
+        if(cnt == 7) {
+            if(tot != 100) return;
+            for(int i = 0; i < DWARFS; i++)
+                if(visit[i]) bw.write(arr[i] + "\n");
+            flag = true; return;
+        }
+        for(int i = 0; i < DWARFS; i++){
+            if(visit[i]) continue;
+            visit[i] = true;
+            dfs(tot + arr[i], cnt + 1);
+            if(flag) return;
+            visit[i] = false;
+        }
+    }
+}
