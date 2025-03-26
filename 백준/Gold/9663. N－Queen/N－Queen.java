a7a5ebcc501f6ffa267b33b50ca8cddb401847f2
suhsein <mini9075@naver.com>
2025-03-26 14:58:55 +0900

    [Gold IV] Title: N-Queen, Time: 368 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,45 +1,41 @@
 import java.util.*;
 import java.io.*;
-import java.lang.Math;
 
 public class Main {
-    static int N, ans;
-    static int[] queens;
-    static BufferedWriter bw;
-    
+    static int N;
+    static int[][] arr;
+    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         N = Integer.parseInt(br.readLine());
-        queens = new int[N];
-
-        nQueens(0);
-        
-        bw.write(Integer.toString(ans));
+      
+        bw.write(Integer.toString(nQueens(0,0,0,0)));
         br.close();
         bw.flush();
         bw.close();
     }
     
-    
-    public static void nQueens(int r) {
-        if(r == N){
-            ans++;
-            return;
+    // 비트마스킹
+    // 2의 보수에 의하여, -x = ~x + 1
+    // center: 동일 열 / left: 좌하향 대각선 / right: 우하향 대각선
+    public static int nQueens(int row, int left, int right, int center){
+        if(row == N) return 1;
+      
+        int ans = 0;
+        // 전체 비트로부터 이미 방문한 비트 제외
+        int unvisited = ((1 << N) - 1) & ~(left | right | center);
+        while(unvisited != 0) {
+            // 현재 행에서 선택한 열 (unvisited 중 가장 오른쪽에 있는 비트 찾기)
+            int curCol = unvisited & -unvisited;
+            // 방문처리
+            unvisited -= curCol;
+            // 좌하향: 다음 행에서는 현재 열 - 1 선택
+            // 우하향: 다음 행에서는 현재 열 + 1 선택
+            ans += nQueens(row + 1, (left | curCol) << 1, (right | curCol) >> 1, (curCol | center));
         }
-        for(int i = 0; i < N; i++) { // col
-            queens[r] = i;
-            if(!visited(r)) nQueens(r + 1); 
-        }
-    }
-
-    public static boolean visited(int r){
-        for(int i = 0; i < r; i++) {
-            // 이전 행들에서 이미 방문한 열. 혹은 대각선 (row diff == col diff)
-            if(queens[i] == queens[r] || Math.abs(i - r) == Math.abs(queens[i] - queens[r]))
-                return true;
-        }
-        return false;
+        
+        return ans;
     }
 }
