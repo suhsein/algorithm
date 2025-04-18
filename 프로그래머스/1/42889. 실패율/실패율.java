620d8df0850e80ff6b3a54597851b7ce0ad72dde
suhsein <mini9075@naver.com>
2025-04-18 16:53:11 +0900

    [level 1] Title: 실패율, Time: 46.80 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,61 +1,52 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        int[] cnt = new int[N + 2];
-        double[] failure = new double[N + 2];
-        Pair[] pairs = new Pair[N + 1];
-        
         Arrays.sort(stages);
-        int x = 1;
+        List<Stage> list = new ArrayList<>();
         
-        for(int i = 0; i < stages.length; i++){
-            while (stages[i] != x){
-                x++;
+        int uncleared = 0, reached = stages.length, idx = 1;
+        
+        for(int s : stages) {
+            while(idx < s) {
+                list.add(new Stage(idx, uncleared, reached));
+                reached -= uncleared;
+                uncleared = 0; idx++;
             }
-            if(stages[i] > N) break;
-            cnt[stages[i]]++;
-            if ((i + 1 < stages.length && stages[i + 1] != x) || i + 1 == stages.length) {
-                failure[stages[i]] = (double)cnt[stages[i]] / (double)(stages.length - i + cnt[stages[i]] - 1);
-                pairs[stages[i]] = new Pair(failure[stages[i]], stages[i]);
-            }
+            uncleared++;
+        }
+        while(idx <= N) {
+            list.add(new Stage(idx++, uncleared, reached));
+            uncleared = 0;
         }
         
-        pairs[0] = new Pair(0.0, N + 1);
+        Collections.sort(list);
+        int n = list.size();
+        int[] answer = new int[n];
         
-        for(int i = 1; i <= N; i++) {
-            if(pairs[i] == null) {
-                pairs[i] = new Pair(failure[i], i);
-            }
-        }
-        Arrays.sort(pairs);
+        for(int i = 0; i < n; i++) 
+            answer[i] = list.get(i).stageNum;
         
-        for(int i = 0; i < N; i++) {
-            answer[i] = pairs[i].y;
-        }
-       
         return answer;
     }
-}
-
-class Pair implements Comparable<Pair> {
-    double x;
-    int y;
-    
-    public Pair(double x, int y){
-        this.x = x;
-        this.y = y;
-    }
-    
-    public int compareTo(Pair p){
-        if(this.x > p.x){
-            return -1;
-        } else if(this.x == p.x) {
-            if(this.y < p.y){
-                return -1;
-            }
+    public static class Stage implements Comparable<Stage> {
+        int stageNum;
+        int uncleared;
+        int reached;
+        
+        public Stage(int stageNum, int uncleared, int reached){
+            this.stageNum = stageNum;
+            this.uncleared = uncleared;
+            this.reached = reached;
         }
-        return 1;
+        
+        public int compareTo(Stage s){
+            double tf = (double)this.uncleared / this.reached;
+            double sf = (double)s.uncleared / s.reached;
+            
+            if(tf == sf) return this.stageNum - s.stageNum;
+            return Double.compare(sf, tf);
+        }
     }
 }
