df314bee09dddf1c369a22112ecbc574da5a2cab
suhsein <mini9075@naver.com>
2025-04-18 12:34:13 +0900

    [level 2] Title: 오픈채팅방, Time: 110.94 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,52 +1,63 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public String[] solution(String[] record) {
         int len = record.length;
-        Map<String, String> m = new HashMap<>(); 
-        int[] arr = new int[len]; // 1, -1
-        String[] ord = new String[len];
+        
+        Map<String, String> m = new HashMap<>();
+        List<Pair> list = new ArrayList<>();
         
         StringTokenizer st; 
-        int idx = 0, cnt = 0;
-        String status, id, name;
+        int idx = 0, cnt = 0, status = 0;
+        String statusString, id, name;
+        
         for(String s : record) {
             st = new StringTokenizer(s, " ");
            
-            status = st.nextToken();
+            statusString = st.nextToken();
             id = st.nextToken();
             
-            if(status.equals("Enter")) {
-                arr[idx] = 1;
+            if(statusString.equals("Enter")) {
                 name = st.nextToken();
                 m.put(id, name);
-                ord[idx] = id;
+                status = 1; cnt++;
+            } else if(statusString.equals("Leave")){
+                status = -1;
                 cnt++;
-            } else if(status.equals("Leave")){
-                arr[idx] = -1;
-                ord[idx] = id;
-                cnt++;
-            } else if(status.equals("Change")) {
-                arr[idx] = 0;
+            } else if(statusString.equals("Change")) {
                 name = st.nextToken();
                 m.put(id, name);
+                status = 0;
             }
-            idx++;
+            
+            list.add(new Pair(status, id));
         }
         
         String[] answer = new String[cnt];
         
-        idx = 0;
+        String enter = "님이 들어왔습니다.";
+        String leave = "님이 나갔습니다.";
+        
         for(int i = 0; i < len; i++) {
-            if(arr[i] == 0) continue;
-            String cur = m.get(ord[i]);
-            cur += "님이 ";
-            if(arr[i] == 1) cur += "들어왔습니다.";
-            else if(arr[i] == -1) cur += "나갔습니다.";
+            Pair p = list.get(i);
+            if(p.status == 0) continue;
+            String cur = m.get(p.id);
+            if(p.status == 1) cur += enter;
+            else if(p.status == -1) cur += leave;
             answer[idx++] = cur;
         }
         
         return answer;
     }
     
+    public static class Pair {
+        int status;
+        String id;
+        
+        public Pair(int status, String id) {
+            this.status = status;
+            this.id = id;
+        }
+    }
 }
