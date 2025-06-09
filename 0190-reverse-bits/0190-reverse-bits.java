21c2d3d6de8c91bb62a2ea2bcf6962c690a49fd3
suhsein <mini9075@naver.com>
2025-06-09 14:20:19 +0900

    Time: 0 ms (100%), Space: 41.7 MB (86.46%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,17 +1,13 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        String s = Integer.toBinaryString(n);
-        int bits = 32 - s.length();
-        StringBuilder sb = new StringBuilder();
+        int answer = 0;
 
-        for(int i = s.length() - 1; i >= 0; i--) 
-            sb.append(s.charAt(i));
-        
-        int answer = Integer.parseUnsignedInt(sb.toString(), 2);
-
-        while(bits-- > 0) 
+        for(int i = 0; i < 32; i++) {
             answer <<= 1;
+            answer |= (n & 1);
+            n >>= 1;    
+        }
         
         return answer;
     }
