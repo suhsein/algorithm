a33bc56d6e1ddfb25a4221f8abf78dfc7ceb27ce
suhsein <mini9075@naver.com>
2025-06-09 18:10:54 +0900

    Time: 1 ms (99.37%), Space: 41.6 MB (97.2%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -4,10 +4,15 @@ class Solution {
         int n = s.length();
         boolean[] dp = new boolean[n + 1];
 
+        int max = 0;
+        for(String w : wordDict)
+            if(w.length() > max) 
+                max = w.length();
+
         dp[0] = true; // empty string ""
 
         for(int i = 1; i <= n; i++) {
-            for(int j = i - 1; j >= 0; j--) {
+            for(int j = i - 1; j >= Math.max(0, i - max); j--) {
                 if(dp[j] && set.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
