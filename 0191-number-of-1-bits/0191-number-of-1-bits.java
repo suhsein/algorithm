09955c6979efa1d3ccd23355beb67a256e6aa923
suhsein <mini9075@naver.com>
2025-06-09 12:28:18 +0900

    Time: 0 ms (100%), Space: 40.5 MB (85.25%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
 
         while(n > 0) {
-            answer += (n & 1);
-            n >>= 1;
+            n &= (n - 1);
+            answer++;
         }
         
         return answer;
