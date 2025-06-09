c4e65cca85d67276c4b006a3baa92469dd118272
suhsein <mini9075@naver.com>
2025-06-09 10:28:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,7 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int lo = 0, hi = n - 1, mid;
+        int pivot = findPivot(nums);
+
+        int idx = binarySearch(nums, target, 0, pivot - 1);
+        if(idx > 0 && idx < pivot && nums[idx] == target) 
+            return idx;
+        idx = binarySearch(nums, target, pivot, nums.length - 1);
+        if(idx > 0 && idx < nums.length && nums[idx] == target) 
+            return idx;
+
+        return -1;
+    }
+
+    public int findPivot(int[] nums) {
+        int lo = 0, hi = nums.length - 1, mid;
 
         while(lo < hi) {
             mid = (lo + hi) / 2;
@@ -12,28 +24,20 @@ class Solution {
                 hi = mid;
         }
 
-        int pivot = hi;
-        
-        int answer = binarySearch(nums, 0, pivot - 1, target);
-        if(answer >= 0 && answer < n && nums[answer] == target)
-            return answer;
-        
-        answer = binarySearch(nums, pivot, n - 1, target);
-        if(answer >= 0 && answer < n && nums[answer] == target)
-            return answer;
-
-        return -1;
+        return hi;
     }
 
-    public int binarySearch(int[] nums, int lo, int hi, int target) {
-        while(lo < hi){
-            int mid = (lo + hi) / 2;
+    public int binarySearch(int[] nums, int target, int lo, int hi) {
+        int mid;
+
+        while(lo < hi) {
+            mid = (lo + hi) / 2;
 
             if(nums[mid] == target)
                 return mid;
             if(nums[mid] < target)
                 lo = mid + 1;
-            else 
+            else
                 hi = mid;
         }
 
