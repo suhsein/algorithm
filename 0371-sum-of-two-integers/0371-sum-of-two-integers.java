548c449aa93b80b8d362b425808c8bddb1c3e885
suhsein <mini9075@naver.com>
2025-06-09 12:10:51 +0900

    Time: 0 ms (100%), Space: 40 MB (98.25%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,10 +1,11 @@
 class Solution {
     public int getSum(int a, int b) {
-        if(b == 0) return a;
+        while(b != 0) {
+            int carry = (a & b) << 1;
+            a ^= b;
+            b = carry;
+        }
 
-        int sumWithoutCarry = a ^ b; // XOR 연산 시 두 비트 다르면 1 -> 올림수 제외 더하기 수행
-        int carry = (a & b) << 1;   // 둘 다 1이라면, 올림수가 되므로 1 비트 밀기
-        
-        return getSum(sumWithoutCarry, carry);
+        return a;
     }
 }
