e6850ee5c4b8db2861b3b3858cb7ecd091bb725b
suhsein <mini9075@naver.com>
2025-06-10 11:52:26 +0900

    Time: 2 ms (83.09%), Space: 45.8 MB (44.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,26 +1,16 @@
 class Solution {
-    static boolean[] dp;
-    static int n;
-
     public boolean canJump(int[] nums) {
-        n = nums.length;
-        dp = new boolean[n];
+        int jump = 0;
 
-        dfs(0, nums);
-
-        return dp[n - 1];
-    }
-
-    public void dfs(int curIdx, int[] nums) {
-        dp[curIdx] = true;
-
-        if(curIdx == n - 1)
-            return;
-
-        for(int i = 1; i <= nums[curIdx]; i++) {
-            if(!dp[curIdx + i])
-                dfs(curIdx + i, nums);
-            if(dp[n - 1]) return;
+        for(int x : nums){
+            if(jump < 0)
+                return false;
+            if(x > jump)
+                jump = x;
+            jump--;
         }
+        
+        return true;
     }
+
 }
