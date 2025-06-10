8b80901023125e2585516ca181e1b241412708df
suhsein <mini9075@naver.com>
2025-06-10 14:37:15 +0900

    Time: 10 ms (22.95%), Space: 46.6 MB (55.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,44 +1,17 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (o1, o2) -> {
-            if(o1[0] == o2[0])
-                return o1[1] - o2[1];
-            return o1[0] - o2[0];
-        });
+        Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0]));
 
-        List<Pair> list = new ArrayList<>();
+        List<int[]> answer = new ArrayList<>();
 
-        for(int[] cur : intervals) {
-            if(list.size() == 0 || 
-            list.size() > 0 && !(list.get(list.size() - 1).merge(cur[0], cur[1])))
-                list.add(new Pair(cur[0], cur[1]));
+        for (int[] interval : intervals) {
+            if (answer.isEmpty() || answer.get(answer.size() - 1)[1] < interval[0])
+                answer.add(interval);
+            else 
+                answer.get(answer.size() - 1)[1] 
+                    = Math.max(answer.get(answer.size() - 1)[1], interval[1]);
         }
 
-        return list.stream()
-                    .map(p -> new int[]{p.x, p.y})
-                    .toArray(int[][]::new);
-    }
-
-    public class Pair {
-        int x, y;
-
-        // x y z w -> 안겹침 : z > y
-        // z w x y -> 안겹침 : w < x
-        // x z y w -> 겹침
-
-        public Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        
-        public boolean merge(int z, int w) {
-            if(z > y) return false;
-            if(w < x) return false; 
-
-            this.x = Math.min(x, z);
-            this.y = Math.max(y, w);
-
-            return true;
-        }
+        return answer.toArray(int[][]::new);
     }
 }
