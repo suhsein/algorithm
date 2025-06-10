e2796ec49df356dd8f8aa3b674db8509163eb931
suhsein <mini9075@naver.com>
2025-06-10 14:06:25 +0900

    Time: 27 ms (29.47%), Space: 42.8 MB (73.16%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,48 +19,21 @@ class Node {
 */
 
 class Solution {
+    HashMap<Integer, Node> hm = new HashMap<>();
+
     public Node cloneGraph(Node node) {
         if(node == null) 
             return null;
-        if(node.neighbors.size() == 0)
-            return new Node(1);
-
-        Map<Integer, Node> dict = new HashMap<>();
-        boolean[] visit = new boolean[101];
-        Queue<Node> q = new LinkedList<>();
-
-        Node first = new Node(1);
-        dict.put(1, first);
-        q.add(node);
-
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-            int val = cur.val;
-
-            if(visit[val]) continue;
-            visit[val] = true;
-
-            Node clone = dict.get(val);
-            if(clone == null) {
-                clone = new Node(val);
-                dict.put(val, clone);
-            }
-
-            for(Node n : cur.neighbors) {
-                if(visit[n.val]) continue;
-
-                Node nClone = dict.get(n.val);
-                if(nClone == null) {
-                    nClone = new Node(n.val);
-                    dict.put(n.val, nClone);
-                }
-
-                clone.neighbors.add(nClone);
-                nClone.neighbors.add(clone);
-                q.add(n);
-            }
-        }
         
-        return first;
+        if(hm.containsKey(node.val))
+            return hm.get(node.val);
+        
+        Node cur = new Node(node.val);
+        hm.put(node.val, cur);
+
+        for(Node n : node.neighbors)
+            cur.neighbors.add(cloneGraph(n));
+
+        return cur;
     }
 }
