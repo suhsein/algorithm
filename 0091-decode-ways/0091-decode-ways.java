8a8a67d9e140938f32cbd69d1fd56567654feacf
suhsein <mini9075@naver.com>
2025-06-10 11:12:54 +0900

    Time: 0 ms (100%), Space: 41.6 MB (93.95%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -8,27 +8,19 @@ class Solution {
             return 1;
 
         int[] dp = new int[n];
-
         dp[0] = 1;
-        dp[1] = 1;
-        if(s.charAt(0) >= '3' && s.charAt(1) == '0') 
-            dp[1] = 0;
-        if(s.charAt(0) == '1' && s.charAt(1) > '0'
-            || s.charAt(0) == '2' && s.charAt(1) >= '1' && s.charAt(1) <= '6')
-            dp[1] = 2;
 
-        for(int i = 2; i < n; i++) {
+        for(int i = 1; i < n; i++) {
             char cur = s.charAt(i), prev = s.charAt(i - 1);
             if(cur == '0') {
                 if(prev == '0' || prev >= '3')
                     return 0;
-                dp[i] = dp[i - 2];
+                dp[i] = (i < 2) ? 1 : dp[i - 2];
                 continue;
             } 
-            dp[i] = dp[i - 1];
             if(prev == '1' 
             || prev == '2' && cur >= '0' && cur <= '6') 
-                dp[i] = dp[i - 1] + dp[i - 2];
+                dp[i] = (i < 2) ? 2 : dp[i - 1] + dp[i - 2];
             else
                 dp[i] = dp[i - 1];
         }
